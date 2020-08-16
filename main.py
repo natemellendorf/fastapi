8afce3ff195e310203a17fd8dc917e6b784176de
Nate Mellendorf <nate.mellendorf@gmail.com>
2020-08-16 13:36:40 -0700

    black

--- main.py
+++ main.py
@@ -2,40 +2,46 @@ from os import environ
 from fastapi import FastAPI, Header
 from typing import Optional
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command, napalm_configure, napalm_get, netmiko_send_config, netmiko_save_config
+from nornir.plugins.tasks.networking import (
+    netmiko_send_command,
+    napalm_configure,
+    napalm_get,
+    netmiko_send_config,
+    netmiko_save_config,
+)
 from nornir.plugins.functions.text import print_result
 
 app = FastAPI()
 
+
 def change_host_data(host):
-        host.username = environ.get("FAST_USERNAME")
-        host.password = environ.get("FAST_PASSWORD")
-        host.hostname = environ.get("FAST_HOSTNAME")
-        host.platform = environ.get("FAST_PLATFORM")
-    
+    host.username = environ.get("FAST_USERNAME")
+    host.password = environ.get("FAST_PASSWORD")
+    host.hostname = environ.get("FAST_HOSTNAME")
+    host.platform = environ.get("FAST_PLATFORM")
+
+
 def junos_update_config(task, **kwargs):
     config = kwargs["config"]
 
     task.run(
-        name="Junos - Apply Config",
-        task=napalm_configure,
-        configuration=config,
+        name="Junos - Apply Config", task=napalm_configure, configuration=config,
     )
 
+
 def junos_get_config(task, **kwargs):
-    task.run(
-        name="Junos - Get Info",
-        task=napalm_get, 
-        getters=["get_facts"]
-    )
+    task.run(name="Junos - Get Info", task=napalm_get, getters=["get_facts"])
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/user")
 async def get_current_user(x_vouch_user: Optional[str] = Header(None)):
     return {"X-Vouch-User": x_vouch_user}
+
 
 @app.get("/set/fw/hostname")
 def set_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
@@ -43,19 +49,17 @@ def set_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
     output = {}
 
     output["Requester"] = x_vouch_user
-    
+
     nr = InitNornir(
         core={"num_workers": 4},
         dry_run=True,
         inventory={
             "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-            "options": {
-                "host_file": "templates/hosts.yml"
-            },
-            "transform_function": change_host_data
-        }
+            "options": {"host_file": "templates/hosts.yml"},
+            "transform_function": change_host_data,
+        },
     )
-    
+
     config = f"""
     system {{
         host-name {x_vouch_user};
@@ -68,11 +72,11 @@ def set_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
 
     for host, task_results in r.items():
         print("Start Processing Host - Facts: " + str(host) + "\n")
-        #print(task_results[1].diff)
+        # print(task_results[1].diff)
         output[host] = task_results[1].diff
-        
 
     return {"Result": output}
+
 
 @app.get("/get/fw/hostname")
 def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
@@ -80,17 +84,15 @@ def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
     output = {}
 
     output["Requester"] = x_vouch_user
-    
+
     nr = InitNornir(
         core={"num_workers": 4},
         dry_run=True,
         inventory={
             "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-            "options": {
-                "host_file": "templates/hosts.yml"
-            },
-            "transform_function": change_host_data
-        }
+            "options": {"host_file": "templates/hosts.yml"},
+            "transform_function": change_host_data,
+        },
     )
 
     r = nr.run(junos_get_config)
@@ -100,7 +102,7 @@ def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
     for host, task_results in r.items():
         print("Start Processing Host - Facts: " + str(host) + "\n")
         facts = task_results[1].result
-        #print(facts["get_facts"]["hostname"])
+        # print(facts["get_facts"]["hostname"])
         sn = facts["get_facts"].get("serial_number")
         hostname = facts["get_facts"].get("hostname")
 
