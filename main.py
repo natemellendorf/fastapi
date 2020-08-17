8cc1b6692b643738e5b4c1eb5e02ffe5f7897e2a
Nate Mellendorf <nate.mellendorf@gmail.com>
2020-08-16 21:14:07 -0700

    Black linting

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from os import environ
 from fastapi import FastAPI, Header, Depends
 from typing import Optional
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command, napalm_configure, napalm_get, netmiko_send_config, netmiko_save_config
+from nornir.plugins.tasks.networking import (
+    netmiko_send_command,
+    napalm_configure,
+    napalm_get,
+    netmiko_send_config,
+    netmiko_save_config,
+)
 from nornir.plugins.functions.text import print_result
 from pydantic import BaseModel
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     """
@@ -23,6 +30,7 @@ class Item(BaseModel):
     platform: str
     config: str
 
+
 def change_host_data(host):
     """
     Change device information as needed
@@ -33,6 +41,7 @@ def change_host_data(host):
     host.hostname = environ.get("FAST_HOSTNAME")
     host.platform = environ.get("FAST_PLATFORM")
 
+
 def init_nornir(tasks: list, dry_run: bool = True, **kwargs):
     """
     Create a Nornir instance and run tasks
@@ -41,15 +50,13 @@ def init_nornir(tasks: list, dry_run: bool = True, **kwargs):
     for task in tasks:
 
         nr = InitNornir(
-        core={"num_workers": 4},
-        dry_run=dry_run,
-        inventory={
-            "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-            "options": {
-                "host_file": "templates/hosts.yml"
+            core={"num_workers": 4},
+            dry_run=dry_run,
+            inventory={
+                "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
+                "options": {"host_file": "templates/hosts.yml"},
+                "transform_function": change_host_data,
             },
-            "transform_function": change_host_data
-            }
         )
 
         r = nr.run(task, config=kwargs.get("config"))
@@ -62,25 +69,23 @@ def junos_update_config(task, **kwargs):
     config = kwargs["config"]
 
     task.run(
-        name="Junos - Apply Config",
-        task=napalm_configure,
-        configuration=config,
+        name="Junos - Apply Config", task=napalm_configure, configuration=config,
     )
 
+
 def junos_get_config(task, **kwargs):
-    task.run(
-        name="Junos - Get Info",
-        task=napalm_get, 
-        getters=["get_facts"]
-    )
+    task.run(name="Junos - Get Info", task=napalm_get, getters=["get_facts"])
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/user")
 async def get_current_user(x_vouch_user: Optional[str] = Header(None)):
     return {"X-Vouch-User": x_vouch_user}
+
 
 @app.post("/set/fw/hostname")
 def set_fw_hostname(data: Item, x_vouch_user: Optional[str] = Header(None)):
@@ -95,10 +100,11 @@ def set_fw_hostname(data: Item, x_vouch_user: Optional[str] = Header(None)):
     r = init_nornir(tasks=[junos_update_config], config=data.config)
 
     for host, task_results in r.items():
-        #print(task_results[1].diff)
+        # print(task_results[1].diff)
         output[host] = task_results[1].diff
 
     return {"Result": output}
+
 
 @app.get("/get/fw/hostname")
 def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
@@ -110,7 +116,7 @@ def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
 
     for host, task_results in r.items():
         facts = task_results[1].result
-        #print(facts["get_facts"]["hostname"])
+        # print(facts["get_facts"]["hostname"])
         sn = facts["get_facts"].get("serial_number")
         hostname = facts["get_facts"].get("hostname")
 
