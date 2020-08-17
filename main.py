c8ee0045e093bb26e19175918fc6b1509c397763
Nate Mellendorf <nate.mellendorf@gmail.com>
2020-08-16 22:06:18 -0700

    linting and adding in example async task

--- main.py
+++ main.py
@@ -10,24 +10,36 @@ from nornir.plugins.tasks.networking import (
 )
 from nornir.plugins.functions.text import print_result
 from pydantic import BaseModel
+import asyncio
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    """
-    hostname: FQDN or IP of device\n
-    username: Username to access the device\n
-    password: Password to access the device\n
-    platform: <a href="https://napalm.readthedocs.io/en/latest/support/">NAPALM plugin for the device</a>\n
-    config: Configuration to apply to the device\n
-    """
-
     hostname: str
     username: str
     password: str
     platform: str
     config: str
+
+
+async def task():
+    print("Example async task")
+
+
+async def repeat_task():
+    while True:
+        await asyncio.sleep(5)
+        await task()
+
+
+@app.on_event("startup")
+def repeated_task():
+    """
+    Create a background task to run with FastAPI
+    """
+
+    asyncio.create_task(repeat_task())
 
 
 def change_host_data(host):
@@ -86,16 +98,30 @@ def junos_get_config(task, **kwargs):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello, World!"}
 
 
 @app.get("/user")
 async def get_current_user(x_vouch_user: Optional[str] = Header(None)):
+    """
+    Get the username for the logged in user
+    """
+
     return {"X-Vouch-User": x_vouch_user}
 
 
 @app.post("/set/fw/hostname")
 def set_fw_hostname(data: Item, x_vouch_user: Optional[str] = Header(None)):
+    """
+    Change the hostname of the demo Junos device\\
+    **DEMO**: Device and config changes are hard set by API
+
+    - **hostname**: FQDN or IP of device
+    - **username**: Username to access the device
+    - **password**: Password to access the device
+    - **platform**: <a href="https://napalm.readthedocs.io/en/latest/support/">NAPALM plugin for the device</a>\n
+    - **config**: Configuration to apply to the device
+    """
 
     output = {}
     output["Requester"] = x_vouch_user
@@ -114,6 +140,9 @@ def set_fw_hostname(data: Item, x_vouch_user: Optional[str] = Header(None)):
 
 @app.get("/get/fw/hostname")
 def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
+    """
+    Get the hostname of the demo Junos device
+    """
 
     output = {}
     output["Requester"] = x_vouch_user
@@ -122,11 +151,9 @@ def get_fw_hostname(x_vouch_user: Optional[str] = Header(None)):
 
     for host, task_results in r.items():
         facts = task_results[1].result
-        sn = facts["get_facts"].get("serial_number")
         hostname = facts["get_facts"].get("hostname")
 
         output[host] = {}
-        output[host]["serial_number"] = sn
         output[host]["hostname"] = hostname
 
     return {"Result": output}
